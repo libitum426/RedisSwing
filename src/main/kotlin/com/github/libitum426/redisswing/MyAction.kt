a7be2ea0e85999064c7a8543d7f1ea328d350f79
GitHub Action <action@github.com>
2023-08-05 02:13:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/libitum426/redisswing/MyAction.kt
+++ src/main/kotlin/com/github/libitum426/redisswing/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.libitum426.redisswing
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/libitum426/redisswing/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/libitum426/redisswing/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.libitum426.redisswing.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.libitum426.redisswing.MyBundle
+import com.github.libitum426.redisswing.services.MyProjectService
 import javax.swing.JButton
 
 
